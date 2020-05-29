966d53998f37253198855f776f0a9018d4db747d
anvithahm <54534166+anvithahm@users.noreply.github.com>
2020-05-29 19:53:17 +0530

    Update Armstrong no.c

--- Armstrong no.c
+++ Armstrong no.c
@@ -4,8 +4,6 @@
 int main()
 {
     int num, lastDigit, digits, sum, i, end;
-
-    /* Input upper limit from user */
     printf("Enter upper limit: ");
     scanf("%d", &end);
 
@@ -14,28 +12,14 @@ int main()
     for(i=1; i<=end; i++)
     {
         sum = 0;
-
-        /* Copy the value of num for processing */
         num = i;
-
-        /* Find total digits in num */
         digits = (int) log10(num) + 1;
-
-        /* Calculate sum of power of digits */
         while(num > 0)
         {
-            /* Extract last digit */
             lastDigit = num % 10;
-
-            // Find sum of power of digits
-            // Use ceil() function to overcome any rounding errors by pow()
             sum = sum + ceil(pow(lastDigit, digits));
-
-            /* Remove the last digit */
             num = num / 10;
         }
-
-        /* Check for Armstrong number */
         if(i == sum)
         {
             printf("%d, ", i);
